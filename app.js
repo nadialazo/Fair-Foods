53a44dbd814fdd255feaf7ced33c00dc21ae5262
Nadia <nadialazo118@gmail.com>
2019-06-07 21:44:37 -0400

    v2

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.listen(port, function(){
-    console.log(`Fair Foods is listening on port ${3000}!`);
-});
-
-app.get('/',(req, res) => {
-    res.render('fairFoods.ejs', {'reservations': myArray});
-    });
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+let myOrders = [];
+let message = '';
 
- app.post('/reservations', (req, res) => {
-     myArray.push(req.body.firstName);
+
+
+app.get('/',(req, res) => {
+    res.render('fairFoods.ejs', {reservationDisplay: message });
+    });
+
+    app.use(express.static('public'));
+    app.use(express.static)
+
+app.post('/reservations', (req, res) => {
+    let reservation = {
+        name:req.body.firstName, 
+        bags: req.body.bags
+    }
+     myOrders.push(reservation);
+     console.log(myOrders);
+        message = `Order accepted: ${reservation.name}, ${reservation.bags} bags. Thanks for your order!`;
      res.redirect('/');
  });
 
- let myArray = [];
+ //All orders 
+
+app.get('/allOrders',(req, res) => {
+    res.render('orders.ejs', {myOrders: myOrders});
+    });
+
+
+ app.listen(port, function(){
+    console.log(`Fair Foods is listening on port ${3000}!`);
+});
+
+
 
 
 
